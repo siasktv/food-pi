d9051b2ce1d50f213b516a9de1f095bba9d45136
siasktv <biankmbm92@gmail.com>
2023-03-17 11:24:16 -0400

    Client

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,104 +1,104 @@
-import axios from 'axios';
+import axios from 'axios'
+import url from '../api'
 
 export function getAllRecipes() {
-    return async (dispatch) => {
-        try {
-            const response = await axios.get('/recipes');
-            return dispatch({
-                type: 'GET_ALL_RECIPES',
-                payload: response.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
-    } 
-};
- 
-export function filterRecipesByDiet(payload){
-    return{
-        type: 'FILTER_RECIPES_BY_DIET',
-        payload
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(`${url}/recipes`)
+      return dispatch({
+        type: 'GET_ALL_RECIPES',
+        payload: response.data,
+      })
+    } catch (error) {
+      console.log(error)
     }
-};
+  }
+}
 
-export function orderByScore(payload){
-    return{
-        type:'ORDER_BY_SCORE',
-        payload
-    }
-};
+export function filterRecipesByDiet(payload) {
+  return {
+    type: 'FILTER_RECIPES_BY_DIET',
+    payload,
+  }
+}
 
-export function orderbyScore50(payload){
-    return{
-        type: 'ORDER_BY_50',
-        payload
-    }
-};
+export function orderByScore(payload) {
+  return {
+    type: 'ORDER_BY_SCORE',
+    payload,
+  }
+}
 
-export function orderByAlphabet(payload){
-    return{
-        type: 'ORDER_BY_ALPHABET',
-        payload
-    }
-};
+export function orderbyScore50(payload) {
+  return {
+    type: 'ORDER_BY_50',
+    payload,
+  }
+}
+
+export function orderByAlphabet(payload) {
+  return {
+    type: 'ORDER_BY_ALPHABET',
+    payload,
+  }
+}
 
 export function filterCreated(payload) {
-    return{
-        type: 'FILTER_CREATED',
-        payload
-    }
+  return {
+    type: 'FILTER_CREATED',
+    payload,
+  }
 }
 
-export function getNameRecipes(name){
-    return async function (dispatch){
-        try{
-            var json = await axios.get('/recipes?name=' + name)
-            return dispatch({
-                type: 'GET_NAME_RECIPES',
-                payload: json.data
-            })
-        } catch(error){
-            console.log(error)
-        }
+export function getNameRecipes(name) {
+  return async function (dispatch) {
+    try {
+      var json = await axios.get(`${url}/recipes?name=` + name)
+      return dispatch({
+        type: 'GET_NAME_RECIPES',
+        payload: json.data,
+      })
+    } catch (error) {
+      console.log(error)
     }
-};
+  }
+}
 
 export function getDiets() {
-    return async function (dispatch) {
-        try{
-            const info = await axios.get('/diets')
-            return dispatch({
-                type: 'GET_DIETS',
-                payload: info.data
-            })
-        }catch(error){
-            console.log(error)
-        }
+  return async function (dispatch) {
+    try {
+      const info = await axios.get(`${url}/diets`)
+      return dispatch({
+        type: 'GET_DIETS',
+        payload: info.data,
+      })
+    } catch (error) {
+      console.log(error)
     }
-};
-
-export function postRecipe(payload){
-    return async function (dispatch) {
-        try{
-            const response = axios.post('/recipes', payload)
-            return response;
-        }catch(error) {
-            console.log(error)
-        }
-    }
-};
-
-export function getDetail (id) {
-    return async function(dispatch) {
-        try{
-            const result = await axios.get('/recipes/' + id)
-            return dispatch({
-                type: 'GET_DETAILS',
-                payload: result.data
-            })
-        } catch(error){
-            console.log(error)
-        }
-    }
+  }
 }
 
+export function postRecipe(payload) {
+  return async function (dispatch) {
+    try {
+      const response = axios.post(`${url}/recipes`, payload)
+      return response
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export function getDetail(id) {
+  return async function (dispatch) {
+    try {
+      const result = await axios.get(`${url}/recipes/` + id)
+      return dispatch({
+        type: 'GET_DETAILS',
+        payload: result.data,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
---
+++ client/src/redux/api.js
@@ -0,0 +1,1 @@
+const url = 'food-pi-production-a75b.up.railway.app'
