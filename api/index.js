20e3626605a0e291be160cda136987a0c1abec41
siasktv <biankmbm92@gmail.com>
2023-03-17 11:00:13 -0400

    Server

--- api/index.js
+++ api/index.js
@@ -17,14 +17,15 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require('dotenv').config();
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const port = process.env.PORT || 3001;
+require('dotenv').config()
+const server = require('./src/app.js')
+const { conn } = require('./src/db.js')
+const port = process.env.PORT || 3001
+const ip = '0.0.0.0'
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log(`Server raised in port ${port}`); // eslint-disable-line no-console
-  });
-});
+  server.listen(port, ip, () => {
+    console.log(`Server running at http://${ip}:${port}/`) // eslint-disable-line no-console
+  })
+})
